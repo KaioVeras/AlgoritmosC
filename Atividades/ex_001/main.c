463fd6e2d9ce1eeca9b4fb28a797929c67f0c40d
KaioVeras <kaiooveras@gmail.com>
2025-08-14 14:51:12 -0300

    update

---
+++ Atividades/ex_001/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+	int idade = 0;
+	idade = 18;
+	
+	float altura = 0;
+	altura = 1.75;
+	
+	double peso = 0;
+	peso = 82.1234567;
+	
+	char status = 'A';
+	
+	float imc = peso / (altura * altura);
+	
+	printf("Idade: %d\n", idade);
+	printf("Altura: %.2f\n", altura);
+	printf("Peso: %lf\n", peso);
+	printf("Status: %c\n", status);
+	printf("Imc: %.2lf\n", imc);
+	
+	return 0;
+}
+
+/*
+	-- Desafio --
+	
+	Desenvolva um programa onde ser?o declaradas as vari?veis abaixo e apresente seus valores na tela
+	
+	idade (ex: 89)
+	altura (ex: 1.87)
+	peso (ex: 82.1234567)
+	status (ex: 'a')
+*/
