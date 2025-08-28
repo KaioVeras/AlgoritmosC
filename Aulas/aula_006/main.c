6ed351f60518ceaa5da6c128cc6f8b1ba31435db
KAIO VÍTOR PINHEIRO VERAS <kaio.veras@a.ucb.br>
2025-08-28 12:56:00 -0300

    aula006

---
+++ Aulas/aula_006/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+	int n1 = 0, n2 =0;
+	
+	printf("--- Comparador ---\n\n");
+	
+	printf("N1: ");
+	scanf("%d", &n1);
+	
+	printf("N2: ");
+	scanf("%d", &n2);
+	
+	if(n1 == n2) {
+		printf("\nn1 == n2");
+	} else {
+		if(n1 > n2) {
+			if(n1%2 == 0){
+				printf("\nN1: par!");
+			} else {
+				printf("\nN1: impar!");
+			}	
+		} else {
+			if(n2%2 == 0){
+				printf("\nN2: par!");
+			} else {
+				printf("\nN2: impar!");
+			}
+		}
+	}	
+	
+	return 0;
+}
+
+/*
+	Operadores?
+		Aritméticos?
+		+ - * / %
+		
+		Atribuição?
+		=
+		
+		Comparação?
+		== != > < >= <=
+		
+		Lógicos?
+		&& || !
+*/
